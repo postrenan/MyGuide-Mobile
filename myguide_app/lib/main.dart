eca32d2b4010beed5b3e1982d6cc0329a3893088
HRiarte <hriartec@gmail.com>
2024-10-28 17:33:19 -0300

    Login Visual
    Icon App
    Splashscreen
    Visual Login (not functional)

--- myguide_app/lib/main.dart
+++ myguide_app/lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'screen_login.dart';
 
 void main() {
-	runApp(const MyApp());
+  runApp(MyApp());
 }
-class MyApp extends StatelessWidget {
-	const MyApp({super.key});
 
-	@override
-	Widget build(BuildContext context) {
-		return const Placeholder();
-	}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyGuide',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:
+          LoginScreen(), // Cargará directamente la pantalla de inicio de sesión
+    );
+  }
 }
